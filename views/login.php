5d8aebc257f27e953344ade3133fb31b9023e6b0
MARCO12355KO <marcogenriquez5@gmail.com>
2026-02-09 22:23:02 +0000

    actualizar

--- views/login.php
+++ views/login.php
@@ -54,8 +54,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Acceso | UNIOR</title>
-    <link rel="icon" type="image/png" href="../assets/img/logo_unior1.png">
-    <link rel="stylesheet" href="../config/assets/css/style.css">
+    <link rel="icon" type="image/png" href="..config/assets/img/logo_unior1.png">
+    <link rel="stylesheet" href="/config/assets/css/style.css">
 </head>
 <body>
 
--- views/menu.php
+++ views/menu.php
@@ -31,7 +31,7 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>UNIOR - Universidad Privada de Oruro</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
-    <link rel="icon" type="image/png" href="../assets/img/logo_unior1.png">
+    <link rel="icon" type="image/png" href="..config/assets/img/logo_unior1.png">
     <link href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:wght@300;500;800&family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
